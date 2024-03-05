77cccab7a6eb69e250e3009c747d325aa739ed3d
Oluwaseyifunmi Oyefeso <gistkiosk@gmail.com>
2024-03-05 13:31:49 +0100

    Added loading on navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,20 @@
 	import '../styles/global.css';
 	import 'iconify-icon';
 	import { Toaster } from 'svelte-sonner';
+	import { navigating } from '$app/stores';
 </script>
 
 <Toaster richColors />
+{#if !!$navigating}
+	<div
+		class="fixed left-0 top-0 z-[9999] flex h-screen w-screen items-center justify-center text-white"
+	>
+		<div class="absolute left-0 top-0 h-full w-full bg-black/30" />
+		<div class="z-50 bg-primary p-5">
+			<iconify-icon icon="eos-icons:bubble-loading" width="40" s />
+		</div>
+	</div>
+{/if}
 
 <div class="relative flex min-h-screen flex-col text-foreground">
 	<Navbar />
